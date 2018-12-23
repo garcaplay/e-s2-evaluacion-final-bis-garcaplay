c24a96134bf1e1bb67f9d122bf6d4ca940b5a140
Silvia García Cacho <silvia@garca.info>
2018-12-23 12:14:53 +0100

    Generate as many lists as radio-value is

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,35 +1,44 @@
 'use strict';
 
-//vamos a desarrollar el código base para inyectar las listas
+//vamos a recoger el valor del radio seleccionado y a hacer que nos pinte las mismas listas que su valor
+const radioSelector = document.querySelectorAll('.form__size-option');
+let listNumber = '';
+('.form__size-option');
+
 const btnSelector = document.querySelector('.form__button');
 
 const ulSelector = document.querySelector('.body__card-list');
 
 function generateLi(){
 
-  const liGenerator = document.createElement('li');
+  ulSelector.innerHTML='';
 
-  const textGenerator = document.createTextNode('blablabla');
+  for(let i=0; i<radioSelector.length; i++){
+    if(radioSelector[i].checked === true){
+      let radioValue = radioSelector[i].value;
+      listNumber = parseInt(radioValue);
+    }
+  }
 
-  liGenerator.appendChild(textGenerator);
+  for (let i=0; i<listNumber; i++){
+    const liGenerator = document.createElement('li');
 
-  liGenerator.classList.add('card-list__element');
+    liGenerator.classList.add('card-list__element');
 
-  const imgGenerator = document.createElement('img');
+    const imgGenerator = document.createElement('img');
 
-  imgGenerator.classList.add('card-list__image');
+    imgGenerator.classList.add('card-list__image');
 
-  liGenerator.appendChild(imgGenerator);
+    imgGenerator.src = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
 
-  ulSelector.appendChild(liGenerator);
+    liGenerator.appendChild(imgGenerator);
+
+    ulSelector.appendChild(liGenerator);
+  }
 
 }
 
 btnSelector.addEventListener('click', generateLi);
-
-
-//<li class="card-list__element"><img class="card-list__image" src="https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB"></li>
-
 
 //Click sobre el botón de 'Comenzar'--> recogemos el valor del radio y se pone en la URL del API es https://raw.githubusercontent.com/Adalab/cards-data/master/NUMERO.json,//Guardamos el número seleccionado en LocalStorage para que al recargar la página aparezca seleccionado el que se usó la última vez
 //Hacemos la petición al API y pintamos en la lista las imágenes
