8fe139277d27c404b9bbd826ca27b4d136a345aa
Silvia García Cacho <silvia@garca.info>
2018-12-23 11:28:38 +0100

    Generate the script to be followed

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,4 @@
 'use strict';
-
-console.log('>> Ready :)');
+//Click sobre el botón de 'Comenzar'--> recogemos el valor del radio y se pone en la URL del API es https://raw.githubusercontent.com/Adalab/cards-data/master/NUMERO.json,//Guardamos el número seleccionado en LocalStorage para que al recargar la página aparezca seleccionado el que se usó la última vez
+//Hacemos la petición al API y pintamos en la lista las imágenes
+//Por defecto se verá solo la parte trasera de las cartas, cuando clickemos sobre una de ellas se verá la parte frontal (y viceversa -toggle-)
