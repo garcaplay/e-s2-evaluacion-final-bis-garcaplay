f6b0a72750fde3eb27a42169094824e7e9a51bcf
Silvia García Cacho <silvia@garca.info>
2018-12-25 11:28:48 +0100

    Try to put background from new api (not working JS func due to order)

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,6 +6,8 @@ const btnSelector = document.querySelector('.form__button');
 
 const ulSelector = document.querySelector('.body__card-list');
 
+const mainPageSelector = document.querySelector('.main__container');
+
 let radioValue = '';
 
 let radioID = '';
@@ -27,6 +29,11 @@ let pokeIDArray = [];
 let pokeImageArray = [];
 
 let pairCompareArray = [];
+
+const imgCreator = document.createElement('img');
+imgCreator.classList.add('main__bg');
+mainPageSelector.appendChild(imgCreator);
+const imgBgSelector = document.querySelector('.main__bg');
 
 if(localStorage.getItem('radioID') !== null){
   preSelect = document.querySelector(`#${localStorage.getItem('radioID')}`),
@@ -129,3 +136,23 @@ btnSelector.addEventListener('click', () =>{
   generateLi();
 });
 
+//vamos a intentar poner de fondo una imagen de pixabay que cambie cada vez que reiniciamos la página
+
+//url api https://pixabay.com/api/?key=11098334-b5dd38a9defdba578bee44bfc&q=pokemon&image_type=illustration
+// data.hits -- .largeImageURL
+function randomNumber(max){
+
+  return Math.ceil((Math.random()*max)+1);
+
+}
+
+fetch(`https://pixabay.com/api/?key=11098334-b5dd38a9defdba578bee44bfc&q=pokemon&image_type=illustration`)
+  .then(response => response.json())
+  .then(function(data){
+    const pokeHits = data.hits;
+    const r = randomNumber(pokeHits.length);
+    const pokeBg= pokeHits[r].largeImageURL;
+
+    imgBgSelector.src = `${pokeBg}`;
+
+  });
